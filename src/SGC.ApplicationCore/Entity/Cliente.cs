6dae1115cd93b47da7b4d2740ccc7ab1aecf598b
vagner sabadi <vagnerbuzatta@hotmail.com>
2018-07-25 11:57:38 -0300

    Adicionar arquivos de projeto.

---
+++ src/SGC.ApplicationCore/Entity/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGC.ApplicationCore.Entity
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+
+        }
+
+        public int ClienteId { get; set; }
+
+        public int Nome { get; set; }
+
+        public int CPF { get; set; }
+
+    }
+}
